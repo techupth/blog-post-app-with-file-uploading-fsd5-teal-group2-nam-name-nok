ba49d44e6d331c09f0db280d755052afb7402fae
shnItt1 <144312080+shnItt1@users.noreply.github.com>
2024-01-25 16:29:54 +0700

    add blog-app

--- client/src/pages/RegisterPage.js
+++ client/src/pages/RegisterPage.js
@@ -6,6 +6,15 @@ function RegisterPage() {
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
   const [password, setPassword] = useState("");
+  const [avatars, setAvatars] = useState({});
+
+  const handleFileChange = (event) => {
+    const uniqueId = Date.now();
+    setAvatars({
+      ...avatars,
+      [uniqueId]: event.target.file[0],
+    });
+  };
 
   const { register } = useAuth();
 
@@ -19,6 +28,25 @@ function RegisterPage() {
     };
     register(data);
   };
+
+  const handleRemoveImage = (event, avatarKey) => {
+    event.preventDefault();
+    delete avatars[avatarKey];
+    setAvatars({ ...avatars });
+  };
+
+  const formData = new FormData();
+
+  formData.append("username", username);
+  formData.append("password", password);
+  formData.append("firstName", firstName);
+  formData.append("lastName", lastName);
+
+  for (let avatarKey in avatars) {
+    formData.append("avatar", avatars[avatarKey]);
+  }
+
+  register(formData);
 
   return (
     <div className="register-form-container">
@@ -85,7 +113,7 @@ function RegisterPage() {
           </label>
         </div>
         <div className="input-container">
-          <label>
+          <label htmlFor="avatar">
             Avatar
             <input
               id="avatar"
@@ -93,10 +121,32 @@ function RegisterPage() {
               type="file"
               placeholder="Enter last name here"
               multiple
-              onChange={(event) => {}}
+              // hidden
+              onChange={handleFileChange}
             />
           </label>
+          <div className="image-list-preview-container">
+            {Object.keys(avatars).map((avatarKey) => {
+              const file = avatars[avatarKey];
+              return (
+                <div key={avatarKey} className="image-preview-container">
+                  <img
+                    className="image-preview"
+                    src={URL.createObjectURL(file)}
+                    alt={file.name}
+                  />
+                  <button
+                    className="image-remove-button"
+                    onClick={(event) => handleRemoveImage(event, avatarKey)}
+                  >
+                    x
+                  </button>
+                </div>
+              );
+            })}
+          </div>
         </div>
+
         <div className="form-actions">
           <button type="submit">Submit</button>
         </div>
